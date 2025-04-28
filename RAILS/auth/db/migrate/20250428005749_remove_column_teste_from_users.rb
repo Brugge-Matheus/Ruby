bcb43844fe19adbd96fa21878525403077c1d83e
Matheus Brugge <matheusmilczwski@gmail.com>
2025-04-28 13:24:27 -0300

    authorization

---
+++ RAILS/auth/db/migrate/20250428005749_remove_column_teste_from_users.rb
@@ -0,0 +1,5 @@
+class RemoveColumnTesteFromUsers < ActiveRecord::Migration[8.0]
+  def change
+    remove_column :users, :teste
+  end
+end
--- RAILS/auth/db/schema.rb
+++ RAILS/auth/db/schema.rb
@@ -10,17 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_26_155101) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_28_010737) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.boolean "isAdmin", default: false, null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -29,4 +35,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_26_155101) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "articles", "users"
 end
